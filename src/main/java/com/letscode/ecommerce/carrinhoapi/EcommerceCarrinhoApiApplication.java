c36d3cf295f980825d4e5ba7d3e791e7fc8b1c5b
jamtibaes <jamtibaes@yahoo.com.br>
2022-05-27 22:13:56 -0300

    Initial commit

---
+++ src/main/java/com/letscode/ecommerce/carrinhoapi/EcommerceCarrinhoApiApplication.java
@@ -0,0 +1,13 @@
+package com.letscode.ecommerce.carrinhoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCarrinhoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceCarrinhoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/letscode/ecommerce/carrinhoapi/EcommerceCarrinhoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letscode.ecommerce.carrinhoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceCarrinhoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
